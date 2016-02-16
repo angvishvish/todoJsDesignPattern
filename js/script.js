6ac44ef13d4dcc4f46ae7f1ddd69070ba3344cf6
angvishvish <angvish@promobitech.com>
2016-02-16 19:53:54 +0530

    adds a working new task

--- js/script.js
+++ js/script.js
@@ -4,33 +4,73 @@
   var todoObject = function () {
     var _taskList = [],
         _instance,
-        _todoListId = 'todo-list',
-        _newTaskText = 'new-task';
+        _todoListId   = document.getElementById('todo-list'),
+        _newTaskText  = document.getElementById('new-task'),
+        _taskCounter  = document.getElementById('task-counter');
+
+    function _init () {
+      this._todoListId  = _todoListId;
+      this._newTaskText = _newTaskText;
+    }
+
+    function _uniqueId () {
+      return (new Date().getTime()).toString(36);
+    }
+
+    function _checkInput () {
+      return _newTaskText.value == '';
+    }
 
     function _addTask () {
-      var div     = document.getElementById(_todoListId),
-          newTask = document.getElementById(_newTaskText),
-          listHtml = '<li>'
-                      '<div class="checkbox">'
-                        '<label>'
-                          '<input type="checkbox"> <span> ' + newTask + '</span>'
-                        '</label>'
-                      '</div>'
-                    '</li>';
+      if (!_checkInput()) {
+        var listHtml  = '<li id="taskId-' + _uniqueId() + '">';
+            listHtml += '<div class="checkbox">';
+            listHtml += '<label>';
+            listHtml += '<input type="checkbox"> <span> ' + _newTaskText.value + '</span>';
+            listHtml += '</label>';
+            listHtml += '<i onclick="removeTask(event)" class="ion-close-circled icon-remove pull-right"></i>';
+            listHtml += '</div>';
+            listHtml += '</li>';
 
-      div.innerHTML += listHml;
-      // this._taskList.
-      console.log('Everything is working fine.')
+        _todoListId.innerHTML += listHtml;
+        _taskList.push(_newTaskText.value);
+
+        _updateTaskCounter();
+        _emptyInput();
+      }
+    }
+
+    function _emptyInput () {
+      _newTaskText.value = '';
+    }
+
+    function _updateTaskCounter () {
+      _taskCounter.innerHTML = '(' + _taskList.length + ')';
+    }
+
+    function _removeTask() {
+      // .parentNode.id;
+      console.log()
     }
 
     return {
-      addTask: _addTask
+      init        : _init,
+      checkInput  : _checkInput,
+      addTask     : _addTask,
+      removeTask  : _removeTask
     }
   }
 
-  function addNewTask (event) {
+  var task = todoObject();
+
+  window.newTaskKeypress = function (event) {
+
+    if(event.charCode == 13)
+      task.addTask(event);
+  }
+
+  window.removeTask = function (event) {
     event.preventDefault();
-    var task = todoObject();
-    task.addTask();
+    task.removeTask();
   }
 })(window, jQuery);
